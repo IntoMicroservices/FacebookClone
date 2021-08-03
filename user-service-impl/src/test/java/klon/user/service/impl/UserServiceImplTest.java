7cb547e8a8c385362f6ae4e8c155f7ece1aea61b
psieniawski <piotr.sieniawski@gmail.com>
2021-08-03 20:16:08 +0200

    Test added, module finished

--- user-service-impl/src/test/java/klon/user/service/impl/UserServiceImplTest.java
+++ user-service-impl/src/test/java/klon/user/service/impl/UserServiceImplTest.java
@@ -7,11 +7,16 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -43,15 +48,41 @@ public class UserServiceImplTest {
 
     @Test
     @SneakyThrows
-    void addUserShouldThrowWhenRepoThrows(){
+    void addUserShouldThrowWhenRepoThrows() {
         User serviceUser = User.builder().userId(randomAlphanumeric(10)).build();
         klon.user.repo.api.User repoUser = klon.user.repo.api.User.builder().userId(serviceUser.getUserId()).build();
         when(mapperMock.toRepoUser(eq(serviceUser))).thenReturn(repoUser);
 
         doThrow(new klon.user.repo.api.UserExistsException("userId")).when(repoMock).addUser(any());
 
-        Assertions.assertThrows(UserExistsException.class,()->service.addUser(serviceUser));
+        Assertions.assertThrows(UserExistsException.class, () -> service.addUser(serviceUser));
     }
 
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void existsShouldReturnWhatRepoReturns(boolean exists) {
+        String username = randomAlphanumeric(10);
+        when(repoMock.exists(eq(username))).thenReturn(exists);
+
+        Assertions.assertEquals(exists, service.exists(username));
+    }
+
+    @Test
+    void getUserShouldReadFromRepoMapAndReturn() {
+        String username = randomAlphanumeric(10);
+        klon.user.repo.api.User repoUser = klon.user.repo.api.User.builder().userId(username).build();
+        User serviceUser = User.builder().userId(username).build();
+
+        when(repoMock.getUser(eq(username))).thenReturn(Optional.of(repoUser));
+        when(mapperMock.toServiceUser(eq(repoUser))).thenReturn(serviceUser);
+
+        Assertions.assertEquals(serviceUser, service.getUser(username).get());
+    }
+
+    @Test
+    void getUserShouldReturnEmptyOptionalIfUserDoesNotExist() {
+        when(repoMock.getUser(anyString())).thenReturn(Optional.empty());
+        assertTrue(service.getUser("sdfasdfads").isEmpty());
+    }
 
 }
