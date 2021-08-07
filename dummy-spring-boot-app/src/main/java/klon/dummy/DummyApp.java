a57396baef4882c2efb0330bbf1b7807c7d5c3e8
psieniawski <piotr.sieniawski@gmail.com>
2021-08-08 00:08:19 +0200

    Disabling sonar for dummy classes

--- dummy-spring-boot-app/src/main/java/klon/dummy/DummyApp.java
+++ dummy-spring-boot-app/src/main/java/klon/dummy/DummyApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @SuppressWarnings("all")//disable sonar for the class
 public class DummyApp {
+    @SuppressWarnings("all")//disable sonar for the class
     public static void main(String[] args) {
         SpringApplication.run(DummyApp.class, args);
     }
