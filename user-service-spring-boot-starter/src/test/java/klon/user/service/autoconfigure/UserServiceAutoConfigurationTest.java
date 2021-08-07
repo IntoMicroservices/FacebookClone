d6797c1f290e76902f1fc0dbeb1814b116b42950
psieniawski <piotr.sieniawski@gmail.com>
2021-08-08 01:18:31 +0200

    Test fixes

--- user-service-spring-boot-starter/src/test/java/klon/user/service/autoconfigure/UserServiceAutoConfigurationTest.java
+++ user-service-spring-boot-starter/src/test/java/klon/user/service/autoconfigure/UserServiceAutoConfigurationTest.java
@@ -8,7 +8,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceAutoConfigurationTest {
+class UserServiceAutoConfigurationTest {
 
     @Mock
     UserRepository userRepo;
