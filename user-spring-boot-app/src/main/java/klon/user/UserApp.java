dc89d834746c462f4b34b91ae4111ee8d21b71a1
Wawrzyniec-Nowak <nowak.wawrzyniec@gmail.com>
2021-08-10 09:16:14 +0200

    user-rest-api initial commit (#3)
    user-service-rest-spring-api base version

--- user-spring-boot-app/src/main/java/klon/user/UserApp.java
+++ user-spring-boot-app/src/main/java/klon/user/UserApp.java
@@ -1,12 +1,12 @@
-package klon.dummy;
+package klon.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DummyApp {
+public class UserApp {
     public static void main(String[] args) {
-        SpringApplication.run(DummyApp.class, args);
+        SpringApplication.run(UserApp.class, args);
     }
 
 }
