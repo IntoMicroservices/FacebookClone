fd65d072ee1745995f24fbff0107ace61c624863
psieniawski <piotr.sieniawski@gmail.com>
2021-08-29 01:06:42 +0200

    WIP

--- big_bang.sh
+++ big_bang.sh
@@ -4,5 +4,10 @@ set -e
 PROJECT_ROOT="$(pwd)"
 
 source ./scripts/local-registry.sh up
-mvn clean install
+mvn clean install -P docker-images
+
+source ./scripts/the-system.sh up
+read -p "Press enter to shutdown and cleanup everything"
+source ./scripts/the-system.sh down
+
 source ./scripts/local-registry.sh down
---
+++ scripts/the-system.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCKER_DIR=$PROJECT_ROOT/docker
+
+if [ "$1" == "up" ]; then
+  docker-compose -f $DOCKER_DIR/the-system.yml -p the-system up -d
+else if [ "$1" == "down" ]; then
+    docker-compose -f $DOCKER_DIR/the-system.yml -p the-system down -v
+  else
+    echo "Usage: `basename $0` <up|down>"
+    exit 1;
+  fi
+fi
+
+
