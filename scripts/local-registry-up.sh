62df1f5487426d6850d7b637a60f76b5e92d81b3
psieniawski <piotr.sieniawski@gmail.com>
2021-08-28 18:40:57 +0200

    WIP

--- scripts/local-registry-up.sh
+++ scripts/local-registry-up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname $(readlink -f $0 || greadlink -f $0))
+SCRIPT_DIR=$(dirname $(greadlink -f $0 || readlink -f $0))
 echo $SCRIPT_DIR
 #docker-compose -f $SCRIPT_DIR/local-registry.yml -p local-registry up -d
