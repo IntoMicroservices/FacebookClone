103b2b943a7fdd0ec9cdac4920c9f5a25986b72b
psieniawski <piotr.sieniawski@gmail.com>
2021-08-29 02:29:49 +0200

    WIP

--- scripts/the-system.sh
+++ scripts/the-system.sh
@@ -3,7 +3,7 @@
 DOCKER_DIR=$PROJECT_ROOT/docker
 
 if [ "$1" == "up" ]; then
-  docker-compose -f $DOCKER_DIR/the-system.yml -p the-system up -d
+  docker-compose -f $DOCKER_DIR/the-system.yml pull && docker-compose -f $DOCKER_DIR/the-system.yml -p the-system up -d
 else if [ "$1" == "down" ]; then
     docker-compose -f $DOCKER_DIR/the-system.yml -p the-system down -v
   else
