6f6e3c34ede36107b3cbe31a8f14802fc6f08243
psieniawski <piotr.sieniawski@gmail.com>
2021-09-02 00:38:26 +0200

    Create ConfigServerApplicationTest.java

---
+++ config-server/src/test/java/klon/configserver/ConfigServerApplicationTest.java
@@ -0,0 +1,13 @@
+package klon.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
