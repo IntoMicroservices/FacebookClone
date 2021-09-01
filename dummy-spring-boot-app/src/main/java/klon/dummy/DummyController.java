27ccfb357ee04666bed82e63019fad9b186af029
psieniawski <piotr.sieniawski@gmail.com>
2021-09-01 23:14:12 +0200

    Stupid config server added - reads data from src/main/resources

--- dummy-spring-boot-app/src/main/java/klon/dummy/DummyController.java
+++ dummy-spring-boot-app/src/main/java/klon/dummy/DummyController.java
@@ -1,6 +1,7 @@
 package klon.dummy;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,19 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("${dummy.rest.base:}")
 @Slf4j
 public class DummyController {
+
+    @Value("${dummy.msg.part1}")
+    private String msgP1;
+    @Value("${dummy.msg.part2}")
+    private String msgP2;
+    @Value("${dummy.msg.part3}")
+    private String msgP3;
+
+
+    @GetMapping("/msg")
+    public String msg() throws InterruptedException{
+        return String.format("%s %s %s",msgP1,msgP2,msgP3);
+    }
 
     @GetMapping("/hello")
     public void hello() throws InterruptedException{
