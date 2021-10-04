1e62e58cb6feea657eb464237eee5253256a0279
psieniawski <piotr.sieniawski@gmail.com>
2021-10-04 21:02:28 +0200

    WIP

--- scripts/local-registry.sh
+++ scripts/local-registry.sh
@@ -11,7 +11,7 @@ DOCKER_DIR=$PROJECT_ROOT/docker
 
 if [ "$1" == "up" ]; then
   docker-compose -f "$DOCKER_DIR"/local-registry.yml -p local-registry up -d
-  lauchBrowser http://localhost:7080 docker-registry-browser &
+  launchBrowser http://localhost:7080 docker-registry-browser &
 elif [ "$1" == "down" ]; then
   docker-compose -f "$DOCKER_DIR"/local-registry.yml -p local-registry down -v
 else
