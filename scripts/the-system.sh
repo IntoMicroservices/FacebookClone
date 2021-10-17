388c259053fac191e799029186157679f7883a92
psieniawski <piotr.sieniawski@gmail.com>
2021-10-17 20:12:40 +0200

    - Service Registry added - Dummy service registers with service registry

--- scripts/the-system.sh
+++ scripts/the-system.sh
@@ -6,11 +6,13 @@ DOCKER_DIR=$PROJECT_ROOT/docker
 if [ "$1" == "up" ]; then
   docker-compose -f "$DOCKER_DIR"/the-system.yml pull -q && docker-compose -f "$DOCKER_DIR"/the-system.yml \
     -p the-system up -d
-  launchBrowser http://localhost:9010/actuator/health config-server &
-  launchBrowser http://localhost:9010/dummy-service/default config-server &
+
+  launchBrowser 'http://localhost:10010/add/?name=local_redis\&host=redis\&port=6379' redisinsight &
+  launchBrowser http://localhost:10020/actuator/health config-server &
+  launchBrowser http://localhost:10020/dummy-service/default config-server &
   launchBrowser http://localhost:9020/actuator/health user-service &
-  launchBrowser http://localhost:9999/actuator/health dummy-service &
-  launchBrowser 'http://localhost:9050/add/?name=local_redis\&host=redis\&port=6379' redisinsight &
+  launchBrowser http://localhost:9010/actuator/health dummy-service &
+  launchBrowser http://localhost:10030 service-registry &
   feedRedis redis &
 
 elif [ "$1" == "down" ]; then
