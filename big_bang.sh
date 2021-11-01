2e517199f6d9ac0294c8f486ebbea888078af221
psieniawski <piotr.sieniawski@gmail.com>
2021-11-01 15:21:49 +0100

    buil/deploy/run pipeline scripts restructurised

--- big_bang.sh
+++ big_bang.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))
+SCRIPT_DIRS=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))/scripts
 
-source "$SCRIPT_DIR"/scripts/functions.sh
+source "$SCRIPT_DIRS"/local-registry.sh up
+source "$SCRIPT_DIRS"/microservice_infrastructure.sh build
+source "$SCRIPT_DIRS"/microservice_infrastructure.sh up
+source "$SCRIPT_DIRS"/microservices.sh build
+source "$SCRIPT_DIRS"/microservices.sh up
 
-source "$SCRIPT_DIR"/scripts/local-registry.sh up
-(cd "$SCRIPT_DIR"; mvn clean install -P docker-images)
-
-source "$SCRIPT_DIR"/scripts/the-system.sh up
 
 read -p "************ Press enter to shutdown and cleanup everything ******************"
-source "$SCRIPT_DIR"/scripts/the-system.sh down
+
+
+source "$SCRIPT_DIRS"/microservices.sh down
+source "$SCRIPT_DIRS"/microservice_infrastructure.sh down
+source "$SCRIPT_DIRS"/local-registry.sh down
+
 
 #remove our images from local docker cache
-docker rmi $(docker images --filter=reference="localhost:5000/*" -q)
-
-source "$SCRIPT_DIR"/scripts/local-registry.sh down
+#docker rmi $(docker images --filter=reference="localhost:5000/*" -q)
--- scripts/microservices.sh
+++ scripts/microservices.sh
@@ -5,3 +5,21 @@ DOCKER_DIR="$SCRIPTS_DIR"/../docker
 
 source "$SCRIPTS_DIR"/functions.sh
 
+if [ "$1" == "up" ]; then
+  docker-compose -f "$DOCKER_DIR"/microservices.yml pull -q && docker-compose -f "$DOCKER_DIR"/microservices.yml \
+    -p microservices up -d
+
+  launchBrowser http://localhost:9020/actuator/health user-service &
+  launchBrowser http://localhost:9010/actuator/health dummy-service &
+
+elif [ "$1" == "down" ]; then
+    docker-compose -f "$DOCKER_DIR"/microservices.yml -p microservices down -v
+elif [ "$1" == "build" ]; then
+#images are pushed to local registry
+  (cd "$SCRIPTS_DIR"/.. || exit; mvn -pl dummy-spring-boot-app,user-spring-boot-app -am install -P docker-images || exit)
+
+else
+    echo "Usage: $(basename "$0") <up|down>"
+    exit 1;
+fi
+
